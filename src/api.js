02e21a52b80e5dc5ea3494fae94b916e9aa86d76
Ogunbowale Uthman <hotmann442@gmail.com>
2025-01-13 01:13:11 +0100

    update endpoint

--- src/api.js
+++ src/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api/v1/";
-
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: "https://jobmag.onrender.com/api/v1/",
 });
 
 api.interceptors.request.use(
